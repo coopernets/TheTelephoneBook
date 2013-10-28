c99012eca40ca184e33ba533cc24c7e23fe602f8
James Laptop <james@coopernets.co.uk>
2013-10-28 14:02:20 +0000

    Commit

--- TheTelephoneBook/Program.cs
+++ TheTelephoneBook/Program.cs
@@ -12,17 +12,51 @@ namespace TheTelephoneBook
         public static string[] phoneNumberArray = new string[20];
         static void Main(string[] args)
         {
-            int menuSelect;
+            Menu();
 
+        }
+        public static void NewRecord()
+        {
+            Console.WriteLine("                           New Record Creator                           ");
+            Console.WriteLine("Please press enter to create a new record and press escape when finished");
+            Console.Write(new string('-', Console.WindowWidth));
+            int indexMem = 0;
+            int i = 0;
+            ConsoleKeyInfo ck;
+            ck = Console.ReadKey();
+            while (ck.Key != ConsoleKey.Escape)
+            {
+                Console.Write("Please enter the full name: ");
+                string nameTemp = Console.ReadLine();
+                Console.Write("Please enter the Phone number: ");
+                string phoneTemp = Console.ReadLine();
+                nameArray[i] = nameTemp;
+                phoneNumberArray[i] = phoneTemp;
+                indexMem = i;
+                i++;
+                ck = Console.ReadKey();
+            }
+
+
+            Menu();
+
+
+
+
+
+        }
+        public static void Menu()
+        {
+            int menuSelect;
+            Console.Clear();
             Console.WriteLine("Welcome to The Phone Book");
-            Console.WriteLine("");
-            Console.WriteLine("");
+            Console.Write(new string('-', Console.WindowWidth));
             Console.WriteLine("1. Create New Record");
             Console.WriteLine("2. Delete Record");
             Console.WriteLine("3. List Records");
             Console.WriteLine("4. Exit the Phone Book");
             Console.WriteLine("");
-            Console.WriteLine("Please select a menu Option: ");
+            Console.Write("Please select a menu Option: ");
             menuSelect = int.Parse(Console.ReadLine());
             switch (menuSelect)
             {
@@ -32,36 +66,36 @@ namespace TheTelephoneBook
                     break;
                 case 2:
                     Console.Clear();
-                    Console.ReadLine();
+                    DeleteRecord();
                     break;
                 case 3:
                     Console.Clear();
-                    Console.ReadLine();
+                    ListRecords();
                     break;
                 case 4:
                     Environment.Exit(0);
                     break;
             }
-
         }
-        static void NewRecord()
+        public static void ListRecords()
         {
-            Console.WriteLine("        New Record Creator        ");
-            Console.Write(new string('-', Console.WindowWidth));
-            int indexMem = 0;
-            for (int i = indexMem; i < 20; i++)
+            for (int i = 0; i < nameArray.Count(); i++)
             {
-                Console.Write("Please enter the full name: ");
-                string nameTemp = Console.ReadLine();
-                Console.Write("Please enter the Phone number: ");
-                string phoneTemp = Console.ReadLine();
-                nameArray[i] = nameTemp;
-                phoneNumberArray[i] = phoneTemp;
-                indexMem = i;
+                if (nameArray[i] != null)
+                {
+                    Console.WriteLine(nameArray[i] + " | " + phoneNumberArray[i]);
+                }
+
+
             }
             Console.ReadLine();
+            Menu();
+        }
+        public static void DeleteRecord()
+        {
 
         }
+
     }
 
 }
